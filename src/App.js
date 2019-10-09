bc7d62bc5f2b616d1ebcabc208adcfd264c8b2ae
Nicole Restrup <nicole.restrup@klarna.com>
2019-10-09 18:03:02 +0200

    all things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import getRandomInt from './getRandomInt';
+import MyHeader from './MyHeader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const facts = [
+  "Cute",
+  "Fun",
+  "Unpredictable",
+  "Nice",
+  "Charming",
+  "Dynamic",
+  "Fearless",
+]
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selected: []
+    }
+  }
+
+  plusHandler = () => {
+    const selected = this.state.selected.slice()
+    const factsNotYetSelected = facts.filter(function(fact) {
+      if (selected.indexOf(fact) === -1)
+        return true
+      else 
+        return false
+    })
+    const randomSelectedFact = factsNotYetSelected[getRandomInt(0, factsNotYetSelected.length)]
+    selected.push(randomSelectedFact)
+    this.setState({
+      selected: selected,
+    })
+  }
+
+  removeHandler = () => {
+    const selected = [...this.state.selected]
+    selected.pop()
+    this.setState({
+      selected: selected,
+    })
+  }
+
+  isRemoveHandlerDisabled = () => {
+    const selected = this.state.selected
+    if (selected.length > 0) return false
+    else return true
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MyHeader/>
+      <h2>
+          Cats are...
+      </h2>
+        <div className="buttonContainer">
+          <button className="buttonStyle" onClick={this.plusHandler}>+</button>
+          <button className="buttonStyle" disabled={this.isRemoveHandlerDisabled()} onClick={this.removeHandler}>-</button>
+        </div>
+        <ul>
+          {this.state.selected.map(fact => (<li>{fact}</li>))}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/MyHeader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function MyHeader() {
+    return <header className="App-header">
+            Fancy cat adjectivisor
+        </header>
+  }
+  export default MyHeader;
